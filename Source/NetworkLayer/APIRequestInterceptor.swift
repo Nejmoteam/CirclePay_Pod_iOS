f82538139e1569e70a3e1a3d22d7598cd35ac51c
adelmohey <79041608+adelmohey@users.noreply.github.com>
2022-05-29 14:09:43 +0200

    update network layer interceptor

--- Source/NetworkLayer/APIRequestInterceptor.swift
+++ Source/NetworkLayer/APIRequestInterceptor.swift
@@ -14,9 +14,9 @@ final class APIRequestInterceptor: RequestInterceptor {
     func adapt(_ urlRequest: URLRequest, for _: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         var urlRequest = urlRequest
         if !CirclePay.merchantToken.isEmpty {
-            urlRequest.headers.add(name: "merchant-token", value: CirclePay.merchantToken)
+            urlRequest.headers.add(name: "merchant-token", value: "Bearer \(CirclePay.merchantToken)")
         }
-        urlRequest.headers.add(name: "account-token", value: CirclePay.accountToken)
+        urlRequest.headers.add(name: "account-token", value: "Bearer \(CirclePay.accountToken)")
         urlRequest.headers.add(name: "account-key", value: CirclePay.accountKey)
         completion(.success(urlRequest))
     }
