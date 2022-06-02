39c9d2f86dc0f46a9007ff329ff7da98a4a6e7f1
a <mahmoudallam@circlepay.ai>
2022-06-02 19:46:29 +0200

    Changed Configs url

--- Source/NetworkLayer/ConfigurationWorker/ConfigurationNetworking.swift
+++ Source/NetworkLayer/ConfigurationWorker/ConfigurationNetworking.swift
@@ -14,6 +14,7 @@ enum ConfigurationNetworking {
 extension ConfigurationNetworking: APIRequestBuilder {
     var baseUrl: String {
         return "https://mocki.io/v1/5f0efbaf-0758-4bf0-9d14-82e3ca228b77"
+      //  return "https://mocki.io/v1/d4908ef0-edc5-4b18-8287-e68da5b4febc"
     }
     var path: String {
         return ""
